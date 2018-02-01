6d932f8890ef6f2cbf09db6ebb99f245899a2f57
Kasper Kofoed <kasp5553@edu.easj.dk>
2018-02-01 15:02:55 +0100

    + Lavet

---
+++ kaffeTests/FlatWhiteTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kaffe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffeTests
+{
+    class FlatWhiteTests
+    {
+            [TestMethod()]
+            public void prisTestFlatWhite()
+            {
+                //Arrange
+                var flatWhite = new FlatWhite();
+
+                //Act
+                int pris = flatWhite.Pris();
+
+                //assert
+                Assert.AreEqual(45, pris);
+
+            }
+        
+
+        [TestMethod()]
+        public void styrkeTestFlatWhite()
+        {
+            //Arrange
+            var flatWhite = new FlatWhite();
+            //Act
+            string styrke = flatWhite.Styrke();
+            //Assert
+            Assert.AreEqual("Mild", styrke);
+        }
+
+        [TestMethod()]
+        public void MililiterTest()
+        {
+            //Arrange
+            var flatWhite = new FlatWhite();
+
+            //Act
+            int mlLiter = flatWhite.MlMælk();
+
+            //assert
+            Assert.AreEqual(160, mlLiter);
+
+        }
+
+    }
+}
